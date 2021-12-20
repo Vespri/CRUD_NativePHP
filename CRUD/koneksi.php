60878ad013a07273c9db3c203178323733fa7e3d
Kresna Vespri Wijaya <83352781+Vespri@users.noreply.github.com>
2021-12-20 13:25:13 +0700

    Delete CRUD directory

--- CRUD/koneksi.php
+++
@@ -1,7 +0,0 @@
-<?php
-$koneksi = new mysqli('localhost', 'root', '','testing');
-
-if ($koneksi->connect_error) {
-    die("Koneksi Gagal: " . $koneksi->connect_error);
-} 
-?>
--- CRUD/readme.md
+++
@@ -1,2 +0,0 @@
-#CRUD Native
-berikut adalah contoh CRUD dengan PHP native dengan mysqli.
